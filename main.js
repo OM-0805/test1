a41a60c6a454e4e87e21c2e5eeed05f3f97d60f3
OM-0805 <miyuo3141@gmail.com>
2025-06-25 20:30:53 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -19,3 +19,34 @@
   play();
 
 }
+{
+  document.addEventListener("DOMContentLoaded", () => {
+    const text = "SCROLL DOWN ";
+    const repeatedText = text.repeat(2);
+    const circleText = document.querySelector(".circle-text_01");
+    const totalChars = repeatedText.length;
+    const anglePerChar = 360 / totalChars;
+
+    for (let i = 0; i < totalChars; i++) {
+      const span = document.createElement("span");
+      span.textContent = repeatedText[i];
+      span.style.transform = `rotate(${i * anglePerChar}deg)`;
+      circleText.appendChild(span);
+    }
+  });
+}
+
+{
+  const container = document.querySelector('.design-container');
+  const prevBtn = document.querySelector('.slider-btn.prev');
+  const nextBtn = document.querySelector('.slider-btn.next');
+
+  prevBtn.addEventListener('click', () => {
+    container.scrollBy({ left: -220, behavior: 'smooth' });
+  });
+
+  nextBtn.addEventListener('click', () => {
+    container.scrollBy({ left: 220, behavior: 'smooth' });
+  });
+
+}
